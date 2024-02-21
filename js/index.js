40477af6e6c5ba557ac800221fa978132c3444af
Nuyễn Tuấn Anh <anh.nt15@citigo.com.vn>
2024-02-21 11:57:08 +0700

    add deleteScoreRound

--- js/index.js
+++ js/index.js
@@ -1,34 +1,36 @@
-let round = 0;
-let totalScore = [];
+var round = 0;
+var totalScore = [];
+var totalData = [];
 
 function createPlayer() {
     let playerNames = document.getElementById("player").value;
-    if(playerNames === '') {
+    if (playerNames === '') {
         alert('Vui lòng nhập tên người chơi');
         return;
-    } 
+    }
     let players = playerNames.split(',');
-    
+
     let playerFormsDiv = document.getElementById("playersForm");
 
     players.forEach((name, index) => {
-      let playerForm = `
+        let playerForm = `
       <label id="playerName${index}" >${name}</label> 
       <input type="number" id="player${index}" name="player${index}}"> 
       <br>
     `;
 
-      playerFormsDiv.innerHTML += playerForm;
+        playerFormsDiv.innerHTML += playerForm;
     });
 
-    playerFormsDiv.innerHTML += `<button id="scoreSubmit" onclick="submitScore()">Xác nhận kết quả trận</button> <br>`
+    playerFormsDiv.innerHTML += `<button id="scoreSubmit" onclick="submitScore()">Xác nhận kết quả trận</button> <br>`;
 
     players.forEach((name) => totalScore.push({ name: name, score: 0 }));
 }
+
 function submitScore() {
     let numPlayer = document.getElementById("player").value.split(',').length;
     let player = [];
-    for(let i = 0; i < numPlayer; i++) {
+    for (let i = 0; i < numPlayer; i++) {
 
         let playersScore = {
             name: document.getElementById(`playerName${i}`).innerText,
@@ -36,23 +38,43 @@ function submitScore() {
         };
         player.push(playersScore);
     }
-    totalScore.forEach((player, index) => {
-        player.score += parseInt(document.getElementById(`player${index}`).value);
-    });
 
-    for(let i = 0; i < numPlayer; i++) { 
+    caculatorTotalScore();
+
+    for (let i = 0; i < numPlayer; i++) {
         document.getElementById(`player${i}`).value = 0;
     }
     showResult(player);
 }
 
 function showResult(resultPlayers) {
+    round++;
     let element = document.getElementById('result');
-    element.innerHTML += `<h4>Trận ${++round}</h4>`
-    resultPlayers.forEach((result) => {
-      let newRow = document.createElement('div');
-      newRow.innerText = `${result.name} : ${result.score}`
-      element.appendChild(newRow);
+    element.innerHTML += `
+    <div id="${round}">
+        <h4 style="display: inline-block">Trận ${round}</h4> 
+        <button onclick="deleteResult('${round}')">X</button>
+        ${resultPlayers.map(result => `<p>${result.name} : ${result.score}</p>`).join('')}
+    </div>`;
+    resultPlayers.forEach(result => totalData.push({ name: result.name, score: result.score, round: round }));
+}
+
+function deleteResult(roundId) {
+    let element = document.getElementById(roundId);
+    element.parentNode.removeChild(element);
+    let scoreRoundDelete = totalData.filter(data => data.round == roundId);
+    totalScore.map((value) => {
+        scoreRoundDelete.forEach((data) => {
+            if (value.name.trim() == data.name.trim()) {
+                value.score -= data.score;
+            }
+        })
+    })
+}
+
+function caculatorTotalScore() {
+    totalScore.forEach((player, index) => {
+        player.score += parseInt(document.getElementById(`player${index}`).value);
     });
 }
 
@@ -60,11 +82,8 @@ function totalScoreLast() {
     let totalScorePlayer = document.getElementById("total");
 
     let playerScoreDiv = "";
-    totalScore.forEach((name, index) => {
-       playerScoreDiv += `
-        <p>${name.name} : ${name.score}</p>
-        `;
-
-      totalScorePlayer.innerHTML = playerScoreDiv;
+    totalScore.forEach((value) => {
+        playerScoreDiv += `<p>${value.name} : ${value.score}</p>`;
+        totalScorePlayer.innerHTML = playerScoreDiv;
     });
 }
